b57006d8094c1bbc52ff5b9c88fe0c3b33ca5ad6
jvdograu01 <jvcarvalho0908@gmail.com>
2025-09-19 12:26:36 -0400

    Add files via upload

---
+++ Ordem de Serviço/src/controller/UsuarioDAO.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+import jdbc.ModuloConexao;
+import view.TelaLogin;
+import view.TelaPrincipal;
+
+/**
+ *
+ * @author clebe
+ */
+public class UsuarioDAO {
+    
+    private Connection conexao;
+    
+    public UsuarioDAO(){
+        this.conexao = ModuloConexao.conectar();
+    }
+    
+    //Metodo efetuaLogin
+    public void efetuaLogin(String email, String senha ) {
+       
+        try {
+
+            //1 passo - SQL
+            String sql = "select * from tbusuarios where usuario = ? and senha = ?";
+            PreparedStatement stmt;
+            stmt = conexao.prepareStatement(sql);
+            stmt.setString(1, email);
+            stmt.setString(2, senha);
+
+            ResultSet rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                //Usuario logou
+                TelaPrincipal tela = new TelaPrincipal();
+                tela.setVisible(true);
+                
+            } else {
+                //Dados incorretos
+                JOptionPane.showMessageDialog(null, "Dados incorretos!");
+                new TelaLogin().setVisible(true);
+            }
+
+        } catch (SQLException erro) {
+            JOptionPane.showMessageDialog(null, "Erro : " + erro);
+        }
+
+    }
+    
+    
+    
+}
